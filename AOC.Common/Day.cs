0b623caa0a3513ec329839b085476384220b6de3
Ben Harris <ben@tilde.team>
2022-10-27 12:46:16 -0400

    add Day.RunFromArgs to remove duplicate functionality in each year

--- AOC.Common/Day.cs
+++ AOC.Common/Day.cs
@@ -10,7 +10,7 @@ public abstract class Day
     }
 
     public static bool UseTestInput { get; set; }
-    
+
     public int Year { get; }
     public int DayNumber { get; }
     public string PuzzleName { get; }
@@ -25,9 +25,9 @@ public abstract class Day
     public abstract object Part1();
     public abstract object Part2();
 
-    public void AllParts(bool verbose = true)
+    private void AllParts(bool verbose = true)
     {
-        Console.WriteLine($"Day {DayNumber,2}: {PuzzleName}");
+        Console.WriteLine($"{Year} Day {DayNumber,2}: {PuzzleName}");
         var s = Stopwatch.StartNew();
         var part1 = Part1();
         s.Stop();
@@ -43,5 +43,31 @@ public abstract class Day
         Console.WriteLine(verbose ? $"{s.ScaleMilliseconds()}ms elapsed" : "");
 
         Console.WriteLine();
+    }
+
+    public static void RunFromArgs(string[] args)
+    {
+        var days = Assembly.GetEntryAssembly()?.GetTypes()
+            .Where(t => t.BaseType == typeof(Day))
+            .Select(t => (Activator.CreateInstance(t) as Day)!)
+            .OrderBy(d => d.DayNumber)
+            .ToList();
+
+        if (days == null || !days.Any())
+        {
+            throw new ApplicationException("no days found");
+        }
+            
+
+        if (args.Length == 1 && int.TryParse(args[0], out var dayNum))
+        {
+            var day = days.FirstOrDefault(d => d.DayNumber == dayNum);
+            if (day != null) day.AllParts();
+            else Console.WriteLine($"Day {dayNum} invalid or not yet implemented");
+        }
+        else
+        {
+            foreach (var d in days) d.AllParts();
+        }
     }
 }
