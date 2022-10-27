7680707f0a1d9609795e688b926bfc6ebd02e295
Ben Harris <ben@tilde.team>
2022-10-27 12:46:16 -0400

    add Day.RunFromArgs to remove duplicate functionality in each year

--- AOC2019/Program.cs
+++ AOC2019/Program.cs
@@ -1,15 +1,1 @@
-﻿var days = Assembly.GetExecutingAssembly().GetTypes()
-    .Where(t => t.BaseType == typeof(Day))
-    .Select(t => (Activator.CreateInstance(t) as Day)!)
-    .OrderBy(d => d.DayNumber);
-
-if (args.Length == 1 && int.TryParse(args[0], out var dayNum))
-{
-    var day = days.FirstOrDefault(d => d.DayNumber == dayNum);
-    if (day != null) day.AllParts();
-    else Console.WriteLine($"Day {dayNum} invalid or not yet implemented");
-}
-else
-{
-    foreach (var d in days) d.AllParts();
-}
+﻿Day.RunFromArgs(args);
