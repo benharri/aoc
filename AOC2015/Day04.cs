ade1148cc060bb73ce7c0915bad7a4a1794dd6fe
Ben Harris <ben@tilde.team>
2022-11-11 16:31:34 -0500

    2015 day 4

--- AOC2015/Day04.cs
+++ AOC2015/Day04.cs
@@ -1,15 +1,44 @@
-﻿namespace AOC2015;
+﻿using System.Security.Cryptography;
+
+namespace AOC2015;
 
 /// <summary>
 /// Day 4: <see href="https://adventofcode.com/2015/day/4"/>
 /// </summary>
 public sealed class Day04 : Day
 {
+    private readonly string _key;
+
     public Day04() : base(2015, 4, "Puzzle Name")
     {
+        _key = Input.First();
     }
 
-    public override object Part1() => "";
+    public override object Part1()
+    {
+        var md5 = MD5.Create();
+        var counter = 0;
 
-    public override object Part2() => "";
+        while (true)
+        {
+            var hash = md5.ComputeHash(Encoding.ASCII.GetBytes(_key + counter));
+            if (BitConverter.ToString(hash).Replace("-", "").StartsWith("00000")) 
+                return counter;
+            counter++;
+        }
+    }
+
+    public override object Part2()
+    {
+        var md5 = MD5.Create();
+        var counter = 0;
+
+        while (true)
+        {
+            var hash = md5.ComputeHash(Encoding.ASCII.GetBytes(_key + counter));
+            if (BitConverter.ToString(hash).Replace("-", "").StartsWith("000000"))
+                return counter;
+            counter++;
+        }
+    }
 }
