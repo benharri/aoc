588f54d878d6037a03d90282db850f045bad1350
Ben Harris <ben@tilde.team>
2022-11-11 16:17:17 -0500

    2015 day 3

--- AOC.Test/Test2015.cs
+++ AOC.Test/Test2015.cs
@@ -8,6 +8,7 @@ public class Test2015
     [DataTestMethod]
     [DataRow(typeof(Day01), "232", "1783")]
     [DataRow(typeof(Day02),"1586300", "3737498")]
+    [DataRow(typeof(Day03), "2081", "2341")]
     public void TestAllDays(Type dayType, string part1, string part2)
     {
         Common.CheckDay(dayType, part1, part2);
@@ -15,6 +16,7 @@ public class Test2015
 
     [DataTestMethod]
     [DataRow(typeof(Day02), "58", "34")]
+    [DataRow(typeof(Day03), "2", "11")]
     public void CheckTestInputs(Type dayType, string part1, string part2)
     {
         Day.UseTestInput = true;
