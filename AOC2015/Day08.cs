faf1fb1d7747d12da1779b69e768cd0c5ea117b9
Ben Harris <ben@tilde.team>
2022-11-13 14:12:09 -0500

    2015 day 8

--- AOC2015/Day08.cs
+++ AOC2015/Day08.cs
@@ -3,13 +3,19 @@
 /// <summary>
 /// Day 8: <see href="https://adventofcode.com/2015/day/8"/>
 /// </summary>
-public sealed class Day08 : Day
+public sealed partial class Day08 : Day
 {
-    public Day08() : base(2015, 8, "Puzzle Name")
+    [GeneratedRegex(@"^""(\\x..|\\.|.)*""$")]
+    private static partial Regex CharSet();
+
+    public Day08() : base(2015, 8, "Matchsticks")
     {
     }
 
-    public override object Part1() => "";
+    int CharCount(string arg) => CharSet().Match(arg).Groups[1].Captures.Count;
+    int EncodedCount(string arg) => 2 + arg.Sum(c => c is '\\' or '\"' ? 2 : 1);
 
-    public override object Part2() => "";
+    public override object Part1() => Input.Sum(line => line.Length) - Input.Sum(CharCount);
+
+    public override object Part2() => Input.Sum(EncodedCount) - Input.Sum(line => line.Length);
 }
