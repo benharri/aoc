514a9028290f4bbd0fa62c03ac8be6a2a3f27c4a
Ben Harris <ben@tilde.team>
2022-12-01 00:31:47 -0500

    2022 day 1

--- AOC.Test/Test2022.cs
+++ AOC.Test/Test2022.cs
@@ -5,7 +5,7 @@ namespace AOC.Test;
 public class Test2022
 {
     [DataTestMethod]
-    [DataRow(typeof(Day01), "", "")]
+    [DataRow(typeof(Day01), "70509", "208567")]
     public void CheckAllDays(Type dayType, string part1, string part2)
     {
         Common.CheckDay(dayType, part1, part2);
