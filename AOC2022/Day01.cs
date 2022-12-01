e5ac78ec0a08b1be4204c8f107df00bb13cec3ea
Ben Harris <ben@tilde.team>
2022-12-01 00:31:47 -0500

    2022 day 1

--- AOC2022/Day01.cs
+++ AOC2022/Day01.cs
@@ -3,19 +3,35 @@
 /// <summary>
 /// Day 1: <see href="https://adventofcode.com/2022/day/1"/>
 /// </summary>
-public sealed class Day01 : Day
+public sealed class Day01 : Day, IDay01
 {
+    private readonly List<List<int>> _elfCalories = new();
+
     public Day01() : base(2022, 1, "Day 1 Puzzle Name")
     {
+        var elf = new List<int>();
+
+        foreach (var line in Input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                _elfCalories.Add(elf);
+                elf = new();
+            }
+            else
+                elf.Add(int.Parse(line));
+        }
+
+        if (elf.Any()) _elfCalories.Add(elf);
     }
 
     public override object Part1()
     {
-        return "";
+        return _elfCalories.OrderByDescending(e => e.Sum()).First().Sum();
     }
 
     public override object Part2()
     {
-        return "";
+        return _elfCalories.OrderByDescending(e => e.Sum()).Take(3).Sum(e => e.Sum());
     }
 }
