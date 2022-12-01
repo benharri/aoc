9860b80ac1b4de85364ef16aa01a98e4a70b75ae
Ben Harris <ben@tilde.team>
2022-12-01 12:40:28 -0500

    use Morelinq.Split() it's better

--- AOC2022/Day01.cs
+++ AOC2022/Day01.cs
@@ -1,37 +1,24 @@
-﻿namespace AOC2022;
+﻿using MoreLinq.Extensions;
+
+namespace AOC2022;
 
 /// <summary>
 /// Day 1: <see href="https://adventofcode.com/2022/day/1"/>
 /// </summary>
 public sealed class Day01 : Day
 {
-    private readonly List<List<int>> _elfCalories = new();
+    private readonly List<List<int>> _elfCalories;
 
     public Day01() : base(2022, 1, "Calorie Counting")
     {
-        var elf = new List<int>();
-
-        foreach (var line in Input)
-        {
-            if (string.IsNullOrWhiteSpace(line))
-            {
-                _elfCalories.Add(elf);
-                elf = new();
-            }
-            else
-                elf.Add(int.Parse(line));
-        }
-
-        if (elf.Any()) _elfCalories.Add(elf);
+        _elfCalories = Input
+            .Split("")
+            .Select(e => e.Select(int.Parse).ToList())
+            .OrderByDescending(e => e.Sum())
+            .ToList();
     }
 
-    public override object Part1()
-    {
-        return _elfCalories.OrderByDescending(e => e.Sum()).First().Sum();
-    }
+    public override object Part1() => _elfCalories.First().Sum();
 
-    public override object Part2()
-    {
-        return _elfCalories.OrderByDescending(e => e.Sum()).Take(3).Sum(e => e.Sum());
-    }
+    public override object Part2() => _elfCalories.Take(3).Sum(e => e.Sum());
 }
