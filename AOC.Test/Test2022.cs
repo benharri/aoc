87f9812283c9295703b8a6790307ac634fe055a3
Ben Harris <ben@tilde.team>
2022-12-03 00:19:20 -0500

    2022 day 3

--- AOC.Test/Test2022.cs
+++ AOC.Test/Test2022.cs
@@ -7,6 +7,7 @@ public class Test2022
     [DataTestMethod]
     [DataRow(typeof(Day01), "70509", "208567")]
     [DataRow(typeof(Day02), "11449", "13187")]
+    [DataRow(typeof(Day03), "7917", "")]
     public void CheckAllDays(Type dayType, string part1, string part2)
     {
         Common.CheckDay(dayType, part1, part2);
@@ -15,6 +16,7 @@ public class Test2022
     [DataTestMethod]
     [DataRow(typeof(Day01), "24000", "45000")]
     [DataRow(typeof(Day02), "15", "12")]
+    [DataRow(typeof(Day03), "157", "70")]
     public void TestAllDays(Type dayType, string part1, string part2)
     {
         Day.UseTestInput = true;
---
+++ AOC2022/Day03.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace AOC2022;
+
+/// <summary>
+/// Day 3: <see href="https://adventofcode.com/2022/day/3"/>
+/// </summary>
+public sealed class Day03 : Day
+{
+    public Day03() : base(2022, 3, "Rucksack Reorganization")
+    {
+    }
+
+    public override object Part1()
+    {
+        var priority = 0;
+        foreach (var line in Input)
+        {
+            var misPacked = line[..(line.Length / 2)].Intersect(line[(line.Length / 2)..]).Single();
+            priority += misPacked - (char.IsUpper(misPacked) ? '&' : '`');
+        }
+
+        return priority;
+    }
+
+    public override object Part2()
+    {
+        var priority = 0;
+        foreach (var group in Input.Chunk(3))
+        {
+            var missing = group[0].Intersect(group[1].Intersect(group[2])).Single();
+            priority += missing - (char.IsUpper(missing) ? '&' : '`');
+        }
+        
+        return priority;
+    }
+}
