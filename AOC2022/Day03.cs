ddafb989fdbc9afe9ca8118e49ac97b6bae70719
Ben Harris <ben@tilde.team>
2022-12-03 16:28:41 -0500

    inline the Selects into Sum

--- AOC2022/Day03.cs
+++ AOC2022/Day03.cs
@@ -9,18 +9,11 @@ public sealed class Day03 : Day
     {
     }
 
-    public override object Part1() => 
-        Input
-            .Select(line => line[..(line.Length / 2)].Intersect(line[(line.Length / 2)..]).Single())
-            .Select(RankItem)
-            .Sum();
+    public override object Part1() =>
+        Input.Sum(line => RankItem(line[..(line.Length / 2)].Intersect(line[(line.Length / 2)..]).Single()));
 
-    public override object Part2() => 
-        Input
-            .Chunk(3)
-            .Select(group => group.Aggregate<IEnumerable<char>>((a, b) => a.Intersect(b)).Single())
-            .Select(RankItem)
-            .Sum();
+    public override object Part2() =>
+        Input.Chunk(3).Sum(group => RankItem(group.Aggregate<IEnumerable<char>>((a, b) => a.Intersect(b)).Single()));
 
     private static int RankItem(char item) => item - (char.IsUpper(item) ? '&' : '`');
 }
