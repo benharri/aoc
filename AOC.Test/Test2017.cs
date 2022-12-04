49b2c5c515f4d44734d01474e142df4680dc00eb
Ben Harris <ben@tilde.team>
2022-12-03 19:36:09 -0500

    Use builtin Enumerable.Chunk() and fix test method naming

--- AOC.Test/Test2017.cs
+++ AOC.Test/Test2017.cs
@@ -7,7 +7,7 @@ public class Test2017
 {
     [DataTestMethod]
     [DataRow(typeof(Day01), "", "")]
-    public void TestAllDays(Type dayType, string part1, string part2)
+    public void CheckAllDays(Type dayType, string part1, string part2)
     {
         Common.CheckDay(dayType, part1, part2);
     }
--- AOC.Test/Test2022.cs
+++ AOC.Test/Test2022.cs
@@ -17,7 +17,7 @@ public class Test2022
     [DataRow(typeof(Day01), "24000", "45000")]
     [DataRow(typeof(Day02), "15", "12")]
     [DataRow(typeof(Day03), "157", "70")]
-    public void TestAllDays(Type dayType, string part1, string part2)
+    public void CheckTestInputs(Type dayType, string part1, string part2)
     {
         Day.UseTestInput = true;
         Common.CheckDay(dayType, part1, part2);
