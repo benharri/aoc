1a8f0499dcb5a883680f6af73ecd79c10e65c7d1
Ben Harris <ben@tilde.team>
2022-12-04 13:15:35 -0500

    2015 day 11

--- AOC2015/Day11.cs
+++ AOC2015/Day11.cs
@@ -5,11 +5,64 @@ namespace AOC2015;
 /// </summary>
 public sealed class Day11 : Day
 {
-    public Day11() : base(2015, 11, "Puzzle Name")
+    private char[] _password;
+
+    public Day11() : base(2015, 11, "Corporate Policy")
     {
+        _password = Input.First().ToCharArray();
     }
 
-    public override object Part1() => "";
+    public override object Part1()
+    {
+        while (!IsValid(ref _password)) Increment(ref _password);
 
-    public override object Part2() => "";
+        return new string(_password);
+    }
+
+    public override object Part2()
+    {
+        Increment(ref _password);
+        while (!IsValid(ref _password)) Increment(ref _password);
+
+        return new string(_password);
+    }
+
+    private static bool IsValid(ref char[] password)
+    {
+        bool check1 = false, check3 = false;
+        for (var j = 2; j < password.Length; j++)
+        {
+            if (password[j] == 'i' || password[j] == 'o' || password[j] == 'l')
+                return false;
+
+            if (password[j - 2] + 1 == password[j - 1] && password[j - 1] + 1 == password[j])
+                check1 = true;
+
+            if (j <= 2) continue;
+
+            for (var k = 0; k + 2 < j; k++)
+                if (password[j - 3 - k] == password[j - 2 - k]
+                    && password[j - 1] == password[j]
+                    && password[j - 2 - k] != password[j - 1])
+                {
+                    check3 = true;
+                }
+        }
+
+        return check1 && check3;
+    }
+
+    private static void Increment(ref char[] password, int at = -1)
+    {
+        while (true)
+        {
+            if (at == -1) at = password.Length - 1;
+
+            password[at]++;
+            if (password[at] == 'i' || password[at] == 'o' || password[at] == 'l') password[at]++;
+            if (password[at] <= 'z') return;
+            password[at] = 'a';
+            at--;
+        }
+    }
 }
