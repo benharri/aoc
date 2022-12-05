a15242aa6e076be9d0447dc95845065fb8333600
Ben Harris <ben@tilde.team>
2022-12-05 12:55:03 -0500

    pull ParseInstruction to a function

--- AOC2022/Day05.cs
+++ AOC2022/Day05.cs
@@ -7,23 +7,22 @@ namespace AOC2022;
 /// </summary>
 public sealed class Day05 : Day
 {
+    private readonly IEnumerable<(int quantity, int from, int to)> _instructions;
     private readonly List<Stack<char>> _stacks;
     private readonly List<Stack<char>> _stacksPart2;
-    private readonly IEnumerable<string> _instructions;
 
     public Day05() : base(2022, 5, "Supply Stacks")
     {
         var s = Input.Split("").ToList();
         var stackDiagram = s[0];
-        _instructions = s[1];
-        var stackCount = stackDiagram.First().Length / 4;
-        _stacks = Enumerable.Range(0, stackCount + 2).Select(_ => new Stack<char>()).ToList();
-        
+        _instructions = s[1].Select(ParseInstruction);
+        _stacks = Enumerable.Range(0, stackDiagram.First().Length / 4 + 2).Select(_ => new Stack<char>()).ToList();
 
         foreach (var line in stackDiagram.Reverse().Skip(1))
         {
             var diagram = line.Chunk(4);
             var diagramIndex = 1;
+
             foreach (var stack in diagram)
             {
                 if (stack[1] != ' ')
@@ -37,39 +36,30 @@ public sealed class Day05 : Day
         _stacks.ForEach(item => _stacksPart2.Add(new(item.Reverse())));
     }
 
+    private static (int quantity, int from, int to) ParseInstruction(string line)
+    {
+        var split = line.Split(' ');
+        return (int.Parse(split[1]), int.Parse(split[3]), int.Parse(split[5]));
+    }
+
     public override object Part1()
     {
-        foreach (var i in _instructions)
-        {
-            var split = i.Split(' ');
-            var quantity = int.Parse(split[1]);
-            var from = int.Parse(split[3]);
-            var to = int.Parse(split[5]);
-
-            for (var j = 0; j < quantity; j++)
-                _stacks[to].Push(_stacks[from].Pop());
-        }
+        foreach (var (quantity, from, to) in _instructions)
+            Enumerable.Range(0, quantity).ForEach(_ => _stacks[to].Push(_stacks[from].Pop()));
 
         return _stacks.Where(q => q.Any()).Aggregate("", (s1, chars) => s1 + chars.Peek());
     }
 
     public override object Part2()
     {
-        foreach (var i in _instructions)
+        foreach (var (quantity, from, to) in _instructions)
         {
-            var split = i.Split(' ');
-            var quantity = int.Parse(split[1]);
-            var from = int.Parse(split[3]);
-            var to = int.Parse(split[5]);
+            var crane = new Stack<char>(quantity);
+            Enumerable.Range(0, quantity).ForEach(_ => crane.Push(_stacksPart2[from].Pop()));
 
-            var temp = new Stack<char>(quantity);
-            for (var j = 0; j < quantity; j++)
-                temp.Push(_stacksPart2[from].Pop());
-
-            while (temp.Any())
-                _stacksPart2[to].Push(temp.Pop());
+            while (crane.Any()) _stacksPart2[to].Push(crane.Pop());
         }
-        
+
         return _stacksPart2.Where(q => q.Any()).Aggregate("", (s1, chars) => s1 + chars.Peek());
     }
 }
