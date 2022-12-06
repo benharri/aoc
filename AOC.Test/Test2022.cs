6954198db3da11772bae6c6f113b7b13bb318b9a
Ben Harris <ben@tilde.team>
2022-12-06 00:09:29 -0500

    2022 day 6

--- AOC.Test/Test2022.cs
+++ AOC.Test/Test2022.cs
@@ -10,6 +10,7 @@ public class Test2022
     [DataRow(typeof(Day03), "7917", "2585")]
     [DataRow(typeof(Day04), "503", "827")]
     [DataRow(typeof(Day05), "TGWSMRBPN", "TZLTLWRNF")]
+    [DataRow(typeof(Day06), "1356", "2564")]
     public void CheckAllDays(Type dayType, string part1, string part2)
     {
         Common.CheckDay(dayType, part1, part2);
@@ -21,6 +22,7 @@ public class Test2022
     [DataRow(typeof(Day03), "157", "70")]
     [DataRow(typeof(Day04), "2", "4")]
     [DataRow(typeof(Day05), "CMZ", "MCD")]
+    [DataRow(typeof(Day06), "7", "19")]
     public void CheckTestInputs(Type dayType, string part1, string part2)
     {
         Common.CheckDay(dayType, part1, part2, true);
