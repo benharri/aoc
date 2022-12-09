3bdf71633dc2fbbee451ca7653d23305be71a031
Ben Harris <ben@tilde.team>
2022-12-09 11:59:43 -0500

    make AllParts() print a bit wider

--- AOC.Common/Day.cs
+++ AOC.Common/Day.cs
@@ -29,18 +29,18 @@ public abstract class Day
 
     private void AllParts(bool verbose = true)
     {
-        Console.Write($"{Year} Day {DayNumber,2}: {PuzzleName,-31}");
-        
+        Console.Write($"{Year} Day {DayNumber,2}: {PuzzleName,-40} ");
+
         var s = Stopwatch.StartNew();
         ProcessInput();
         s.Stop();
         Console.WriteLine($"{s.ScaleMilliseconds()}ms elapsed processing input");
         s.Reset();
-        
+
         s.Start();
         var part1 = Part1();
         s.Stop();
-        Console.Write($"Part 1: {part1,-35} ");
+        Console.Write($"Part 1: {part1,-45} ");
         Console.WriteLine(verbose ? $"{s.ScaleMilliseconds()}ms elapsed" : "");
 
         s.Reset();
@@ -48,7 +48,7 @@ public abstract class Day
         s.Start();
         var part2 = Part2();
         s.Stop();
-        Console.Write($"Part 2: {part2,-35} ");
+        Console.Write($"Part 2: {part2,-45} ");
         Console.WriteLine(verbose ? $"{s.ScaleMilliseconds()}ms elapsed" : "");
 
         Console.WriteLine();
@@ -66,7 +66,6 @@ public abstract class Day
         {
             throw new ApplicationException("no days found");
         }
-            
 
         if (args.Length == 1 && int.TryParse(args[0], out var dayNum))
         {
