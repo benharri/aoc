c03d4149443d83203ea332b85b8c1dedb7072ef7
Ben Harris <ben@tilde.team>
2022-12-09 12:56:12 -0500

    inline MoveTail method

--- AOC2022/Day09.cs
+++ AOC2022/Day09.cs
@@ -15,19 +15,7 @@ public sealed class Day09 : Day
     {
         _steps = Input.Select(line => (line[0], int.Parse(line[2..]))).ToList();
     }
-    
-    private static (int x, int y) MoveTail((int x, int y) tail, (int x, int y) head)
-    {
-        int dx = head.x - tail.x, dy = head.y - tail.y;
-        if (Math.Max(Math.Abs(dx), Math.Abs(dy)) > 1)
-        {
-            tail.x += Math.Sign(dx);
-            tail.y += Math.Sign(dy);
-        }
 
-        return tail;
-    }
-    
     private int CountTailPositions(int ropeLength)
     {
         var rope = Enumerable.Range(0, ropeLength).Select(_ => (x: 0, y: 0)).ToArray();
@@ -35,24 +23,27 @@ public sealed class Day09 : Day
 
         foreach (var step in _steps!)
         {
-            int dx = 0, dy = 0;
-            switch (step.direction)
-            {
-                case 'U': dy = 1;  break;
-                case 'D': dy = -1; break;
-                case 'L': dx = -1; break;
-                case 'R': dx = 1;  break;
-            }
-
             foreach (var _ in Enumerable.Range(0, step.count))
             {
-                rope[0].x += dx;
-                rope[0].y += dy;
+                switch (step.direction)
+                {
+                    case 'U': rope[0].y++;  break;
+                    case 'D': rope[0].y--; break;
+                    case 'L': rope[0].x--; break;
+                    case 'R': rope[0].x++;  break;
+                }
+                
                 foreach (var i in Enumerable.Range(1, ropeLength - 1))
                 {
-                    rope[i] = MoveTail(rope[i], rope[i - 1]);
-                    visited.Add(rope.Last());
+                    int dx = rope[i - 1].x - rope[i].x, dy = rope[i - 1].y - rope[i].y;
+                    if (Math.Max(Math.Abs(dx), Math.Abs(dy)) > 1)
+                    {
+                        rope[i].x += Math.Sign(dx);
+                        rope[i].y += Math.Sign(dy);
+                    }
                 }
+
+                visited.Add(rope.Last());
             }
         }
 
