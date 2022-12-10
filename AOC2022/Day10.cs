9ef937b1bb04abae3741cc390d0cbc7f6ea2f34e
Ben Harris <ben@tilde.team>
2022-12-10 14:40:21 -0500

    use string.Join instead of Aggregate

--- AOC2022/Day10.cs
+++ AOC2022/Day10.cs
@@ -6,7 +6,7 @@ namespace AOC2022;
 public sealed class Day10 : Day
 {
     private readonly List<int> _interestingSignals = new();
-    private List<char>? _charMap;
+    private readonly List<char> _charMap = Enumerable.Range(0, 6 * 40).Select(_ => ' ').ToList();
 
     public Day10() : base(2022, 10, "Cathode-Ray Tube")
     {
@@ -15,7 +15,6 @@ public sealed class Day10 : Day
     public override void ProcessInput()
     {
         int x = 1, cycle = 1;
-        _charMap = Enumerable.Range(0, 6 * 40).Select(_ => ' ').ToList();
 
         foreach (var line in Input)
         {
@@ -38,13 +37,12 @@ public sealed class Day10 : Day
             _interestingSignals.Add(x * cycle);
 
         if (new[] { x - 1, x, x + 1 }.Contains((cycle - 1) % 40))
-            _charMap![cycle] = '█';
+            _charMap[cycle] = '█';
 
         cycle++;
     }
 
     public override object Part1() => _interestingSignals.Sum();
 
-    public override object Part2() =>
-        _charMap!.Chunk(40).Aggregate("", (s, chars) => $"{s}{Environment.NewLine}{new string(chars)}");
+    public override object Part2() => string.Join(Environment.NewLine, _charMap.Chunk(40).Select(s => new string(s)));
 }
