c793b3021f8b6bc9d7251708e66d4cc237abefe0
Ben Harris <ben@tilde.team>
2022-12-10 22:17:09 -0500

    2018 day 3

--- AOC.Test/Test2018.cs
+++ AOC.Test/Test2018.cs
@@ -8,6 +8,7 @@ public class Test2018
     [DataTestMethod]
     [DataRow(typeof(Day01), "582", "488")]
     [DataRow(typeof(Day02), "5166", "cypueihajytordkgzxfqplbwn")]
+    [DataRow(typeof(Day03), "119551", "1124")]
     public void CheckAllDays(Type dayType, string part1, string part2)
     {
         Common.CheckDay(dayType, part1, part2);
