7702e95aac8c533aaa129b39ff424b6915935f0a
Ben Harris <ben@tilde.team>
2022-12-30 23:21:44 -0500

    tidy 2022 day 2
    maybe i'll finish this year's problems who knows

--- AOC2022/Day02.cs
+++ AOC2022/Day02.cs
@@ -5,54 +5,23 @@ namespace AOC2022;
 /// </summary>
 public sealed class Day02 : Day
 {
-    private List<string[]>? _rounds;
+    private int _score1;
+    private int _score2;
 
     public Day02() : base(2022, 2, "Rock Paper Scissors")
     {
     }
 
-    public override void ProcessInput() =>
-        _rounds = Input.Select(line => line.Split(' ')).ToList();
-
-    public override object Part1()
+    public override void ProcessInput()
     {
-        var score = 0;
-        foreach (var round in _rounds!)
+        foreach (var line in Input)
         {
-            var me = round[1][0];
-            var elf = round[0][0];
-            score += me - 'W';
-            
-            if (me == 'X' && elf == 'C' ||
-                me == 'Y' && elf == 'A' ||
-                me == 'Z' && elf == 'B') score += 6;
-            
-            else if (elf - 'A' == me - 'X') score += 3;
+            int a = line[0] - 'A', b = line[2] - 'X';
+            _score1 += 1 + b + (4 + b - a) % 3 * 3;
+            _score2 += 1 + b * 3 + (2 + b + a) % 3;
         }
-
-        return score;
     }
 
-    public override object Part2()
-    {
-        var score = 0;
-        foreach (var round in _rounds!)
-        {
-            var c = round[0][0];
-            switch (round[1][0])
-            {
-                case 'X': // lose
-                    score += "BCA".IndexOf(c) + 1;
-                    break;
-                case 'Y': // draw
-                    score += c - 'A' + 4;
-                    break;
-                case 'Z': // win
-                    score += "CAB".IndexOf(c) + 7;
-                    break;
-            }
-        }
-
-        return score;
-    }
+    public override object Part1() => _score1;
+    public override object Part2() => _score2;
 }
