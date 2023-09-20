dbd011c74253e7d725d07eca93d278f7b80faa28
Ben Harris <ben@tilde.team>
2023-09-20 16:29:42 -0400

    2015 day 19

--- AOC2015/Day19.cs
+++ AOC2015/Day19.cs
@@ -1,3 +1,5 @@
+using MoreLinq;
+
 namespace AOC2015;
 
 /// <summary>
@@ -5,11 +7,42 @@ namespace AOC2015;
 /// </summary>
 public sealed class Day19() : Day(2015, 19, "Medicine for Rudolph")
 {
+    private IEnumerable<string[]>? _rules;
+    private string? _compound;
+
     public override void ProcessInput()
     {
+        var i = Input.Split("").ToList();
+        _rules = i[0].Select(r => r.Split(" => "));
+        _compound = i[1].Single();
     }
 
-    public override object Part1() => "";
+    private int CountSubstring(string needle)
+    {
+        var count = 0;
+        for (var i = _compound!.IndexOf(needle, StringComparison.InvariantCulture);
+             i >= 0;
+             ++count, i = _compound.IndexOf(needle, i + 1, StringComparison.InvariantCulture))
+        {
+        }
 
-    public override object Part2() => "";
+        return count;
+    }
+
+    public override object Part1()
+    {
+        HashSet<string> compounds = new();
+        foreach (var rule in _rules!)
+            foreach (var match in Regex.EnumerateMatches(_compound, rule[0]))
+                compounds.Add(_compound!.Remove(match.Index, rule[0].Length).Insert(match.Index, rule[1]));
+
+        return compounds.Count;
+    }
+
+    public override object Part2() =>
+        _compound!.Count(char.IsUpper)
+        - CountSubstring("Rn")
+        - CountSubstring("Ar")
+        - 2 * CountSubstring("Y")
+        - 1;
 }
