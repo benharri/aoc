382d5368c33bfda01502107c832a44ec51c578e5
Ben Harris <ben@tilde.team>
2023-09-20 13:22:48 -0400

    2015 day 17

--- AOC2015/Day17.cs
+++ AOC2015/Day17.cs
@@ -5,15 +5,26 @@ namespace AOC2015;
 /// </summary>
 public sealed class Day17 : Day
 {
+    private List<int>? _containers;
+    private IEnumerable<List<int>>? _combinations;
+
     public Day17() : base(2015, 17, "No Such Thing as Too Much")
     {
     }
     
     public override void ProcessInput()
     {
+        _containers = Input.Select(int.Parse).ToList();
+        _combinations = Enumerable.Range(1, (1 << _containers.Count) - 1)
+            .Select(i => _containers.Where((_, index) => ((1 << index) & i) != 0).ToList());
     }
 
-    public override object Part1() => "";
+    public override object Part1() => _combinations!.Count(c => c.Sum() == 150);
 
-    public override object Part2() => "";
+    public override object Part2()
+    {
+        var successfulCombinations = _combinations!.Where(c => c.Sum() == 150).ToList();
+        var minCount = successfulCombinations.Min(c => c.Count);
+        return successfulCombinations.Count(c => c.Count == minCount);
+    }
 }
