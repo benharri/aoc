39edb477210bd079ae366e1b7b1b179032bba3b9
Ben Harris <ben@tilde.team>
2023-11-23 11:35:08 -0500

    2016 day 2

--- AOC.Test/Test2016.cs
+++ AOC.Test/Test2016.cs
@@ -7,8 +7,16 @@ public class Test2016
 {
     [DataTestMethod]
     [DataRow(typeof(Day01), "300", "159")]
+    [DataRow(typeof(Day02), "76792", "A7AC3")]
     public void CheckAllDays(Type dayType, string part1, string part2)
     {
         Common.CheckDay(dayType, part1, part2);
+    }
+
+    [DataTestMethod]
+    [DataRow(typeof(Day02), "1985", "5DB3")]
+    public void CheckTestInputs(Type dayType, string part1, string part2)
+    {
+        Common.CheckDay(dayType, part1, part2, true);
     }
 }
