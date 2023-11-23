3303e3e000268777d8745286219ebae462aaee3f
Ben Harris <ben@tilde.team>
2023-11-23 11:43:58 -0500

    add puzzle name

--- AOC2016/Day02.cs
+++ AOC2016/Day02.cs
@@ -3,7 +3,7 @@ namespace AOC2016;
 /// <summary>
 /// Day 2: <a href="https://adventofcode.com/2016/day/2"/>
 /// </summary>
-public sealed class Day02() : Day(2016, 2, "Puzzle Name")
+public sealed class Day02() : Day(2016, 2, "Bathroom Security")
 {
     public override void ProcessInput()
     {
