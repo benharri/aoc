970455b04b754b0955c9904773fdecf67d222fb9
Ben Harris <ben@tilde.team>
2023-11-23 20:17:52 -0500

    2016 day 3 part 2

--- AOC.Test/Test2016.cs
+++ AOC.Test/Test2016.cs
@@ -8,7 +8,7 @@ public class Test2016
     [DataTestMethod]
     [DataRow(typeof(Day01), "300", "159")]
     [DataRow(typeof(Day02), "76792", "A7AC3")]
-    [DataRow(typeof(Day03), "993", "")]
+    [DataRow(typeof(Day03), "993", "1849")]
     public void CheckAllDays(Type dayType, string part1, string part2)
     {
         Common.CheckDay(dayType, part1, part2);
--- AOC2016/Day03.cs
+++ AOC2016/Day03.cs
@@ -5,20 +5,35 @@ namespace AOC2016;
 /// </summary>
 public sealed class Day03() : Day(2016, 3, "Squares With Three Sides")
 {
-    private List<List<int>> _triangles = null!;
+    private List<int[]> _triangles = null!;
 
     public override void ProcessInput()
     {
         _triangles = Input
             .Select(line => line.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse).ToList())
+                .Select(int.Parse).ToArray())
             .ToList();
     }
 
-    public override object Part1() =>
-        _triangles.Count(triangle => triangle[0] + triangle[1] > triangle[2] &&
-                                     triangle[0] + triangle[2] > triangle[1] &&
-                                     triangle[1] + triangle[2] > triangle[0]);
+    public override object Part1() => _triangles.Count(ValidTriangle);
 
-    public override object Part2() => "";
+    private static bool ValidTriangle(params int[] triangle)
+    {
+        return triangle[0] + triangle[1] > triangle[2] &&
+               triangle[0] + triangle[2] > triangle[1] &&
+               triangle[1] + triangle[2] > triangle[0];
+    }
+
+    public override object Part2()
+    {
+        var cnt = 0;
+        foreach (var chunk in _triangles.Chunk(3))
+        {
+            if (ValidTriangle(chunk[0][0], chunk[1][0], chunk[2][0])) cnt++;
+            if (ValidTriangle(chunk[0][1], chunk[1][1], chunk[2][1])) cnt++;
+            if (ValidTriangle(chunk[0][2], chunk[1][2], chunk[2][2])) cnt++;
+        }
+
+        return cnt;
+    }
 }
