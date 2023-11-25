b2dd2cf21571942fe8f5c8280e0924e747a892c3
Ben Harris <ben@tilde.team>
2023-11-25 13:22:25 -0500

    update Test.Common to match the printing style in Aoc.common

--- AOC.Test/Common.cs
+++ AOC.Test/Common.cs
@@ -6,32 +6,29 @@ public static class Common
     {
         Day.UseTestInput = testInput;
         var day = Activator.CreateInstance(dayType) as Day;
+        
         Assert.IsNotNull(day, "failed to instantiate day object");
         Assert.IsTrue(File.Exists(day.FileName), $"File.Exists(day.FileName) {day.FileName}");
-        Console.Write($"{day.Year} Day {day.DayNumber,2}: {day.PuzzleName,-25} ");
-        
+
         var s = Stopwatch.StartNew();
         day.ProcessInput();
         s.Stop();
-        Console.WriteLine($"{s.ScaleMilliseconds()} ms elapsed processing input");
+        
+        Console.WriteLine(
+            $"{day.Year} Day {day.DayNumber,2}: {day.PuzzleName,-40}{s.ScaleMilliseconds()} ms elapsed processing input");
 
         // part 1
-        s.Reset();
-        s.Start();
-        var part1Actual = day.Part1().ToString();
+        s.Restart();
+        var part1Actual = day.Part1();
         s.Stop();
-        Console.Write($"Part 1: {part1Actual,-30} ");
-        Console.WriteLine($"{s.ScaleMilliseconds()} ms elapsed");
-        Assert.AreEqual(part1, part1Actual, $"Incorrect answer for Day {day.DayNumber} Part1");
+        Console.WriteLine($"Part 1: {part1Actual,-45}{s.ScaleMilliseconds()} ms elapsed");
+        Assert.AreEqual(part1, part1Actual.ToString(), $"Incorrect answer for Day {day.DayNumber} Part1");
 
         // part 2
-        s.Reset();
-        s.Start();
-        var part2Actual = day.Part2().ToString();
+        s.Restart();
+        var part2Actual = day.Part2();
         s.Stop();
-        Console.Write($"Part 2: {part2Actual,-30} ");
-        Console.WriteLine($"{s.ScaleMilliseconds()} ms elapsed");
-        Assert.AreEqual(part2, part2Actual, $"Incorrect answer for Day {day.DayNumber} Part2");
-        Day.UseTestInput = testInput;
+        Console.WriteLine($"Part 2: {part2Actual,-45}{s.ScaleMilliseconds()} ms elapsed");
+        Assert.AreEqual(part2, part2Actual.ToString(), $"Incorrect answer for Day {day.DayNumber} Part2");
     }
 }
