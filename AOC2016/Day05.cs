23c3e25b8a48696207d40015810cffc094f64db8
Ben Harris <ben@tilde.team>
2023-11-25 13:22:05 -0500

    2016 day 5

--- AOC2016/Day05.cs
+++ AOC2016/Day05.cs
@@ -1,15 +1,59 @@
+using System.Security.Cryptography;
+
 namespace AOC2016;
 
 /// <summary>
 /// Day 5: <a href="https://adventofcode.com/2016/day/5"/>
 /// </summary>
-public sealed class Day05() : Day(2016, 5, "Puzzle Name")
+public sealed class Day05() : Day(2016, 5, "How About a Nice Game of Chess?")
 {
     public override void ProcessInput()
     {
     }
 
-    public override object Part1() => "";
+    public override object Part1()
+    {
+        var s = Input.First();
+        var answer = new char[8];
+        var index = 0;
 
-    public override object Part2() => "";
+        for (var i = 0; i < answer.Length; i++)
+        {
+            while (true)
+            {
+                var hash = BitConverter.ToString(MD5.HashData(Encoding.ASCII.GetBytes(s + index++))).Replace("-", "");
+                if (hash.StartsWith("00000"))
+                {
+                    answer[i] = hash[5];
+                    break;
+                }
+            }
+        }
+
+        return new string(answer);
+    }
+
+    public override object Part2()
+    {
+        var s = Input.First();
+        var answer = new char[8];
+        var index = 0;
+        var found = 0;
+
+        while (true)
+        {
+            var hash = BitConverter.ToString(MD5.HashData(Encoding.ASCII.GetBytes(s + index++))).Replace("-", "");
+            if (hash.StartsWith("00000"))
+            {
+                var target = hash[5] - '0';
+                if (target is < 8 and >= 0 && answer[target] == 0)
+                {
+                    answer[target] = hash[6];
+                    if (++found == 8) break;
+                }
+            }
+        }
+
+        return new string(answer);
+    }
 }
