76ab5a2d7ad934099b7208fdba1341ce1149ca5d
Ben Harris <ben@tilde.team>
2023-11-25 22:54:09 -0500

    spacing

--- AOC.Test/Common.cs
+++ AOC.Test/Common.cs
@@ -15,20 +15,20 @@ public static class Common
         s.Stop();
         
         Console.WriteLine(
-            $"{day.Year} Day {day.DayNumber,2}: {day.PuzzleName,-40}{s.ScaleMilliseconds()} ms elapsed processing input");
+            $"{day.Year} Day {day.DayNumber,2}: {day.PuzzleName,-40} {s.ScaleMilliseconds()} ms elapsed processing input");
 
         // part 1
         s.Restart();
         var part1Actual = day.Part1();
         s.Stop();
-        Console.WriteLine($"Part 1: {part1Actual,-45}{s.ScaleMilliseconds()} ms elapsed");
+        Console.WriteLine($"Part 1: {part1Actual,-45} {s.ScaleMilliseconds()} ms elapsed");
         Assert.AreEqual(part1, part1Actual.ToString(), $"Incorrect answer for Day {day.DayNumber} Part 1");
 
         // part 2
         s.Restart();
         var part2Actual = day.Part2();
         s.Stop();
-        Console.WriteLine($"Part 2: {part2Actual,-45}{s.ScaleMilliseconds()} ms elapsed");
+        Console.WriteLine($"Part 2: {part2Actual,-45} {s.ScaleMilliseconds()} ms elapsed");
         Assert.AreEqual(part2, part2Actual.ToString(), $"Incorrect answer for Day {day.DayNumber} Part 2");
     }
 }
