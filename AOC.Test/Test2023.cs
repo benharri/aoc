106c78a3871114db9e39489652deac0f8024457f
Ben Harris <ben@tilde.team>
2023-12-03 01:44:25 -0500

    2023 day 3 part 2

--- AOC.Test/Test2023.cs
+++ AOC.Test/Test2023.cs
@@ -8,7 +8,7 @@ public class Test2023
     [DataTestMethod]
     [DataRow(typeof(Day01), "54331", "54518")]
     [DataRow(typeof(Day02), "2476", "54911")]
-    [DataRow(typeof(Day03), "522726", "")]
+    [DataRow(typeof(Day03), "522726", "81721933")]
     public void CheckAllDays(Type dayType, string part1, string part2) =>
         Common.CheckDay(dayType, part1, part2);
 
