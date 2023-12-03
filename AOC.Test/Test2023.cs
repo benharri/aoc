ce841c1bb71e7e4869211764120ca9b10b7bc35d
Ben Harris <ben@tilde.team>
2023-12-02 19:10:37 -0500

    2023 day 2

--- AOC.Test/Test2023.cs
+++ AOC.Test/Test2023.cs
@@ -7,11 +7,13 @@ public class Test2023
 {
     [DataTestMethod]
     [DataRow(typeof(Day01), "54331", "54518")]
+    [DataRow(typeof(Day02), "2476", "54911")]
     public void CheckAllDays(Type dayType, string part1, string part2) =>
         Common.CheckDay(dayType, part1, part2);
 
     [DataTestMethod]
     [DataRow(typeof(Day01), "142", "142")] // unfortunately p2 example is different
+    [DataRow(typeof(Day02), "8", "2286")]
     public void CheckTestInputs(Type dayType, string part1, string part2) =>
         Common.CheckDay(dayType, part1, part2, true);
 }
---
+++ AOC2023/Day02.cs
@@ -0,0 +1,61 @@
+namespace AOC2023;
+
+/// <summary>
+/// Day  2: <a href="https://adventofcode.com/2023/day/ 2"/>
+/// </summary>
+public sealed class Day02() : Day(2023, 2, "Cube Conundrum")
+{
+    public override void ProcessInput()
+    {
+    }
+    
+    private static bool PossibleGame(string line)
+    {
+        var rounds = line.Split(": ", 2)[1];
+        foreach (var round in rounds.Split("; "))
+        {
+            foreach (var draw in round.Split(", "))
+            {
+                var s = draw.Split(' ');
+                var limit = s[1] switch
+                {
+                    "blue" => 14,
+                    "green" => 13,
+                    "red" => 12,
+                    _ => 0,
+                };
+                if (int.Parse(s[0]) > limit) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public override object Part1() =>
+        Input.Where(PossibleGame).Sum(l => int.Parse(l.Split(' ', ':')[1]));
+
+    public override object Part2()
+    {
+        var sum = 0;
+        Dictionary<string, int> mins = new() { { "red", 0 }, { "green", 0 }, { "blue", 0 } };
+        
+        foreach (var line in Input)
+        {
+            foreach (var (name, _) in mins) mins[name] = 0;
+            
+            var rounds = line.Split(": ", 2)[1];
+            foreach (var round in rounds.Split("; "))
+            {
+                foreach (var draw in round.Split(", "))
+                {
+                    var s = draw.Split(' ');
+                    mins[s[1]] = Math.Max(int.Parse(s[0]), mins[s[1]]);
+                }
+            }
+
+            sum += mins.Aggregate(1, (acc, curr) => acc * curr.Value);
+        }
+
+        return sum;
+    }
+}
