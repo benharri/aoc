02537511732371cded8e74955bc7070d9c9116dc
Ben Harris <ben@tilde.team>
2024-05-09 14:35:35 -0400

    apparently i haven't written any of the solutions for 2017

--- AOC.Test/Test2017.cs
+++ AOC.Test/Test2017.cs
@@ -5,7 +5,7 @@ namespace AOC.Test;
 [TestClass]
 public class Test2017
 {
-    [DataTestMethod]
+    //[DataTestMethod]
     //[DataRow(typeof(Day01), "", "")]
     public void CheckAllDays(Type dayType, string part1, string part2) =>
         Common.CheckDay(dayType, part1, part2);
