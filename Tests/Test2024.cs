7882eb3dfdf1418e78b539e7204253ad872a1707
Ben Harris <ben@tilde.team>
2024-12-01 16:05:07 -0500

    2024 day 1

---
+++ Tests/Test2024.cs
@@ -0,0 +1,16 @@
+﻿using Solutions._2024;
+namespace Tests;
+
+[TestClass]
+public class Test2024
+{
+    [DataTestMethod]
+    [DataRow(typeof(Day01), "1319616", "27267728")]
+    public void CheckAllDays(Type dayType, string part1, string part2) =>
+        Common.CheckDay(dayType, part1, part2);
+    
+    [DataTestMethod]
+    [DataRow(typeof(Day01), "11", "31")]
+    public void CheckTestInputs(Type dayType, string part1, string part2) =>
+        Common.CheckDay(dayType, part1, part2, true);
+}
