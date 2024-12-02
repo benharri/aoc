837b25e0657dff4c63874cf38c02e3af93e60109
Ben Harris <ben@tilde.team>
2024-12-02 01:08:35 -0500

    part 2

--- Tests/Test2024.cs
+++ Tests/Test2024.cs
@@ -6,13 +6,13 @@ public class Test2024
 {
     [DataTestMethod]
     [DataRow(typeof(Day01), "1319616", "27267728")]
-    [DataRow(typeof(Day02), "321", "")]
+    [DataRow(typeof(Day02), "321", "386")]
     public void CheckAllDays(Type dayType, string part1, string part2) =>
         Common.CheckDay(dayType, part1, part2);
     
     [DataTestMethod]
     [DataRow(typeof(Day01), "11", "31")]
-    [DataRow(typeof(Day02), "2", "")]
+    [DataRow(typeof(Day02), "2", "4")]
     public void CheckTestInputs(Type dayType, string part1, string part2) =>
         Common.CheckDay(dayType, part1, part2, true);
 }
