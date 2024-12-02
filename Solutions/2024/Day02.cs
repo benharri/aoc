2267eb46e86633e92df161067ce9b83049320a4c
Ben Harris <ben@tilde.team>
2024-12-02 01:08:35 -0500

    part 2

--- Solutions/2024/Day02.cs
+++ Solutions/2024/Day02.cs
@@ -14,5 +14,30 @@ public sealed class Day02() : Day(2024, 2, "")
             .Count(pairs => pairs.All(c => Math.Abs(c) is >= 1 and <= 3) &&
                             (pairs.All(c => c > 0) || pairs.All(c => c < 0)));
 
-    public override object Part2() => "";
+    public override object Part2()
+    {
+        var result = 0;
+        
+        foreach (var line in Input)
+        {
+            var ints = line.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            for (var i = 0; i < ints.Count; i++)
+            {
+                var intsCopy = ints.ToList();
+                intsCopy.RemoveAt(i);
+                
+                var pairs = intsCopy.Pairwise((x, y) => y - x).ToList();
+                if (pairs.All(c => Math.Abs(c) is >= 1 and <= 3) &&
+                    (pairs.All(c => c > 0) || pairs.All(c => c < 0)))
+                {
+                    result++;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
 }
