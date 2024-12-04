98a954733ea8256d23315300b631c2a8c8454bed
Ben Harris <ben@tilde.team>
2024-12-03 19:24:52 -0500

    part 2

--- Solutions/2024/Day03.cs
+++ Solutions/2024/Day03.cs
@@ -5,14 +5,33 @@
 /// </summary>
 public partial class Day03() : Day(2024, 3, "Mull It Over")
 {
+    private string _input = "";
+
     [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)")]
     private static partial Regex MulRegex();
-    
+
+    [GeneratedRegex(@"don't\(\)|do\(\)|mul\((\d{1,3}),(\d{1,3})\)")]
+    private static partial Regex DoDontRegex();
+
+    public override void ProcessInput() => _input = string.Join("", Input);
+
     public override object Part1() =>
-        MulRegex().Matches(string.Join("", Input))
+        MulRegex().Matches(_input)
             .Sum(m => int.Parse(m.Groups[1].ValueSpan) * int.Parse(m.Groups[2].ValueSpan));
 
-    public override object Part2() => "";
-    
-    
+    public override object Part2()
+    {
+        var sum = 0;
+        var enabled = true;
+
+        foreach (Match m in DoDontRegex().Matches(_input))
+        {
+            if (m.ValueSpan is "do()") enabled = true;
+            if (m.ValueSpan is "don't()") enabled = false;
+            if (enabled && m.ValueSpan.StartsWith("mul("))
+                sum += int.Parse(m.Groups[1].ValueSpan) * int.Parse(m.Groups[2].ValueSpan);
+        }
+
+        return sum;
+    }
 }
--- Tests/Test2024.cs
+++ Tests/Test2024.cs
@@ -7,14 +7,14 @@ public class Test2024
     [DataTestMethod]
     [DataRow(typeof(Day01), "1319616", "27267728")]
     [DataRow(typeof(Day02), "321", "386")]
-    [DataRow(typeof(Day03), "163931492", "")]
+    [DataRow(typeof(Day03), "163931492", "76911921")]
     public void CheckAllDays(Type dayType, string part1, string part2) =>
         Common.CheckDay(dayType, part1, part2);
     
     [DataTestMethod]
     [DataRow(typeof(Day01), "11", "31")]
     [DataRow(typeof(Day02), "2", "4")]
-    [DataRow(typeof(Day03), "161", "")]
+    [DataRow(typeof(Day03), "161", "48")]
     public void CheckTestInputs(Type dayType, string part1, string part2) =>
         Common.CheckDay(dayType, part1, part2, true);
 }
