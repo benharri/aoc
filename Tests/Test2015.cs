9b4e218ba1e393b85108f7d6d8dd834c7c1c1603
Ben Harris <ben@tilde.team>
2025-04-21 17:55:49 -0400

    slnx format and move configs to top-level

--- Tests/Test2015.cs
+++ Tests/Test2015.cs
@@ -6,11 +6,11 @@ namespace Tests;
 public class Test2015
 {
     [DataTestMethod]
-    [DataRow(typeof(Day01), "232", "1783")]
-    [DataRow(typeof(Day02), "1586300", "3737498")]
-    [DataRow(typeof(Day03), "2081", "2341")]
-    [DataRow(typeof(Day04), "346386", "9958218")]
-    [DataRow(typeof(Day05), "258", "53")]
+    [DataRow(typeof(Day01NotQuiteLisp), "232", "1783")]
+    [DataRow(typeof(Day02IWasToldThereWouldBeNoMath), "1586300", "3737498")]
+    [DataRow(typeof(Day03PerfectlySphericalHousesInAVacuum), "2081", "2341")]
+    [DataRow(typeof(Day04TheIdealStockingStuffer), "346386", "9958218")]
+    [DataRow(typeof(Day05DoesntHeHaveInternElvesForThis), "258", "53")]
     // [DataRow(typeof(Day06), "543903", "14687245")] // TODO: optimize
     [DataRow(typeof(Day07), "3176", "14710")]
     [DataRow(typeof(Day08), "1342", "2074")]
@@ -35,12 +35,12 @@ public class Test2015
         Common.CheckDay(dayType, part1, part2);
 
     [DataTestMethod]
-    [DataRow(typeof(Day01), "-1", "5")]
-    [DataRow(typeof(Day02), "58", "34")]
-    [DataRow(typeof(Day03), "2", "11")]
+    [DataRow(typeof(Day01NotQuiteLisp), "-1", "5")]
+    [DataRow(typeof(Day02IWasToldThereWouldBeNoMath), "58", "34")]
+    [DataRow(typeof(Day03PerfectlySphericalHousesInAVacuum), "2", "11")]
     // [DataRow(typeof(Day04), "609043", "6742839")]
-    [DataRow(typeof(Day05), "1", "1")]
-    [DataRow(typeof(Day06), "1000000", "1000000")]
+    [DataRow(typeof(Day05DoesntHeHaveInternElvesForThis), "1", "1")]
+    [DataRow(typeof(Day06ProbablyAFireHazard), "1000000", "1000000")]
     // [DataRow(typeof(Day07), "", "")] // test input doesn't have "a" wire
     [DataRow(typeof(Day08), "12", "19")]
     [DataRow(typeof(Day09), "605", "982")]
