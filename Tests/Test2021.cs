6ade85e65c367bc9c82c43a1861fe6d5686e5fd7
Ben Harris <ben@tilde.team>
2025-04-22 15:18:48 -0400

    switch to TUnit

--- Tests/Test2021.cs
+++ Tests/Test2021.cs
@@ -2,23 +2,22 @@ using Solutions._2021;
 
 namespace Tests;
 
-[TestClass]
 public class Test2021
 {
-    [DataTestMethod]
-    [DataRow(typeof(Day01SonarSweep), "1616", "1645")]
-    [DataRow(typeof(Day02Dive), "2272262", "2134882034")]
-    [DataRow(typeof(Day03BinaryDiagnostic), "3009600", "6940518")]
-    [DataRow(typeof(Day04GiantSquid), "8580", "9576")]
-    [DataRow(typeof(Day05HydrothermalVenture), "7318", "19939")]
-    [DataRow(typeof(Day06Lanternfish), "362740", "1644874076764")]
-    [DataRow(typeof(Day07TheTreacheryOfWhales), "345035", "97038163")]
-    [DataRow(typeof(Day08SevenSegmentSearch), "362", "1020159")]
-    [DataRow(typeof(Day09SmokeBasin), "478", "1327014")]
-    [DataRow(typeof(Day10SyntaxScoring), "288291", "820045242")]
-    [DataRow(typeof(Day11DumboOctopus), "1613", "510")]
-    [DataRow(typeof(Day12PassagePathing), "4549", "120535")]
-    [DataRow(typeof(Day13TransparentOrigami), "837",
+    [Test]
+    [Arguments(typeof(Day01SonarSweep), "1616", "1645")]
+    [Arguments(typeof(Day02Dive), "2272262", "2134882034")]
+    [Arguments(typeof(Day03BinaryDiagnostic), "3009600", "6940518")]
+    [Arguments(typeof(Day04GiantSquid), "8580", "9576")]
+    [Arguments(typeof(Day05HydrothermalVenture), "7318", "19939")]
+    [Arguments(typeof(Day06Lanternfish), "362740", "1644874076764")]
+    [Arguments(typeof(Day07TheTreacheryOfWhales), "345035", "97038163")]
+    [Arguments(typeof(Day08SevenSegmentSearch), "362", "1020159")]
+    [Arguments(typeof(Day09SmokeBasin), "478", "1327014")]
+    [Arguments(typeof(Day10SyntaxScoring), "288291", "820045242")]
+    [Arguments(typeof(Day11DumboOctopus), "1613", "510")]
+    [Arguments(typeof(Day12PassagePathing), "4549", "120535")]
+    [Arguments(typeof(Day13TransparentOrigami), "837",
         """
         ████▒███▒▒████▒▒██▒▒█▒▒█▒▒██▒▒█▒▒█▒█▒▒█
         █▒▒▒▒█▒▒█▒▒▒▒█▒█▒▒█▒█▒█▒▒█▒▒█▒█▒▒█▒█▒▒█
@@ -27,35 +26,35 @@ public class Test2021
         █▒▒▒▒█▒▒▒▒█▒▒▒▒█▒▒█▒█▒█▒▒█▒▒█▒█▒▒█▒█▒▒█
         ████▒█▒▒▒▒████▒▒███▒█▒▒█▒▒██▒▒█▒▒█▒▒██▒
         """)]
-    [DataRow(typeof(Day14ExtendedPolymerization), "5656", "12271437788530")]
-    [DataRow(typeof(Day15Chiton), "702", "2955")]
-    [DataRow(typeof(Day16PacketDecoder), "852", "19348959966392")]
-    [DataRow(typeof(Day17TrickShot), "12090", "5059")]
-    [DataRow(typeof(Day18Snailfish), "4289", "4807")]
-    // [DataRow(typeof(Day19BeaconScanner), "338", "9862")] // takes too long and i don't feel like optimizing
-    [DataRow(typeof(Day20TrenchMap), "5306", "17497")]
-    [DataRow(typeof(Day21DiracDice), "512442", "346642902541848")]
-    [DataRow(typeof(Day22ReactorReboot), "658691", "1228699515783640")]
-    [DataRow(typeof(Day23Amphipod), "15365", "52055")]
-    [DataRow(typeof(Day24ArithmeticLogicUnit), "99299513899971", "93185111127911")]
-    [DataRow(typeof(Day25SeaCucumber), "417", "")]
+    [Arguments(typeof(Day14ExtendedPolymerization), "5656", "12271437788530")]
+    [Arguments(typeof(Day15Chiton), "702", "2955")]
+    [Arguments(typeof(Day16PacketDecoder), "852", "19348959966392")]
+    [Arguments(typeof(Day17TrickShot), "12090", "5059")]
+    [Arguments(typeof(Day18Snailfish), "4289", "4807")]
+    // [Arguments(typeof(Day19BeaconScanner), "338", "9862")] // takes too long and i don't feel like optimizing
+    [Arguments(typeof(Day20TrenchMap), "5306", "17497")]
+    [Arguments(typeof(Day21DiracDice), "512442", "346642902541848")]
+    [Arguments(typeof(Day22ReactorReboot), "658691", "1228699515783640")]
+    [Arguments(typeof(Day23Amphipod), "15365", "52055")]
+    [Arguments(typeof(Day24ArithmeticLogicUnit), "99299513899971", "93185111127911")]
+    [Arguments(typeof(Day25SeaCucumber), "417", "")]
     public void CheckAllDays(Type dayType, string part1, string part2) =>
-        Common.CheckDay(dayType, part1, part2);
+        _ = Common.CheckDay(dayType, part1, part2);
 
-    [DataTestMethod]
-    [DataRow(typeof(Day01SonarSweep), "7", "5")]
-    [DataRow(typeof(Day02Dive), "150", "900")]
-    [DataRow(typeof(Day03BinaryDiagnostic), "198", "230")]
-    [DataRow(typeof(Day04GiantSquid), "4512", "1924")]
-    [DataRow(typeof(Day05HydrothermalVenture), "5", "12")]
-    [DataRow(typeof(Day06Lanternfish), "5934", "26984457539")]
-    [DataRow(typeof(Day07TheTreacheryOfWhales), "37", "168")]
-    [DataRow(typeof(Day08SevenSegmentSearch), "26", "61229")]
-    [DataRow(typeof(Day09SmokeBasin), "15", "1134")]
-    [DataRow(typeof(Day10SyntaxScoring), "26397", "288957")]
-    [DataRow(typeof(Day11DumboOctopus), "1656", "195")]
-    [DataRow(typeof(Day12PassagePathing), "226", "3509")]
-    [DataRow(typeof(Day13TransparentOrigami), "17",
+    [Test]
+    [Arguments(typeof(Day01SonarSweep), "7", "5")]
+    [Arguments(typeof(Day02Dive), "150", "900")]
+    [Arguments(typeof(Day03BinaryDiagnostic), "198", "230")]
+    [Arguments(typeof(Day04GiantSquid), "4512", "1924")]
+    [Arguments(typeof(Day05HydrothermalVenture), "5", "12")]
+    [Arguments(typeof(Day06Lanternfish), "5934", "26984457539")]
+    [Arguments(typeof(Day07TheTreacheryOfWhales), "37", "168")]
+    [Arguments(typeof(Day08SevenSegmentSearch), "26", "61229")]
+    [Arguments(typeof(Day09SmokeBasin), "15", "1134")]
+    [Arguments(typeof(Day10SyntaxScoring), "26397", "288957")]
+    [Arguments(typeof(Day11DumboOctopus), "1656", "195")]
+    [Arguments(typeof(Day12PassagePathing), "226", "3509")]
+    [Arguments(typeof(Day13TransparentOrigami), "17",
         """
         █████
         █▒▒▒█
@@ -63,17 +62,17 @@ public class Test2021
         █▒▒▒█
         █████
         """)]
-    [DataRow(typeof(Day14ExtendedPolymerization), "1588", "2188189693529")]
-    [DataRow(typeof(Day15Chiton), "40", "315")]
-    [DataRow(typeof(Day16PacketDecoder), "16", "15")]
-    [DataRow(typeof(Day17TrickShot), "45", "112")]
-    [DataRow(typeof(Day18Snailfish), "4140", "3993")]
-    [DataRow(typeof(Day19BeaconScanner), "79", "3621")]
-    [DataRow(typeof(Day20TrenchMap), "35", "3351")]
-    [DataRow(typeof(Day21DiracDice), "739785", "444356092776315")]
-    [DataRow(typeof(Day22ReactorReboot), "590784", "39769202357779")]
-    [DataRow(typeof(Day23Amphipod), "12521", "44169")]
-    [DataRow(typeof(Day25SeaCucumber), "58", "")]
+    [Arguments(typeof(Day14ExtendedPolymerization), "1588", "2188189693529")]
+    [Arguments(typeof(Day15Chiton), "40", "315")]
+    [Arguments(typeof(Day16PacketDecoder), "16", "15")]
+    [Arguments(typeof(Day17TrickShot), "45", "112")]
+    [Arguments(typeof(Day18Snailfish), "4140", "3993")]
+    [Arguments(typeof(Day19BeaconScanner), "79", "3621")]
+    [Arguments(typeof(Day20TrenchMap), "35", "3351")]
+    [Arguments(typeof(Day21DiracDice), "739785", "444356092776315")]
+    [Arguments(typeof(Day22ReactorReboot), "590784", "39769202357779")]
+    [Arguments(typeof(Day23Amphipod), "12521", "44169")]
+    [Arguments(typeof(Day25SeaCucumber), "58", "")]
     public void CheckTestInputs(Type dayType, string part1, string part2) =>
-        Common.CheckDay(dayType, part1, part2, true);
+        _ = Common.CheckDay(dayType, part1, part2, true);
 }
--- Tests/Test2024.cs
+++ Tests/Test2024.cs
@@ -2,22 +2,21 @@ using Solutions._2024;
 
 namespace Tests;
 
-[TestClass]
 public class Test2024
 {
-    [DataTestMethod]
-    [DataRow(typeof(Day01HistorianHysteria), "1319616", "27267728")]
-    [DataRow(typeof(Day02RedNosedReports), "321", "386")]
-    [DataRow(typeof(Day03MullItOver), "163931492", "76911921")]
-    [DataRow(typeof(Day04CeresSearch), "2573", "1850")]
+    [Test]
+    [Arguments(typeof(Day01HistorianHysteria), "1319616", "27267728")]
+    [Arguments(typeof(Day02RedNosedReports), "321", "386")]
+    [Arguments(typeof(Day03MullItOver), "163931492", "76911921")]
+    [Arguments(typeof(Day04CeresSearch), "2573", "1850")]
     public void CheckAllDays(Type dayType, string part1, string part2) =>
-        Common.CheckDay(dayType, part1, part2);
+        _ = Common.CheckDay(dayType, part1, part2);
 
-    [DataTestMethod]
-    [DataRow(typeof(Day01HistorianHysteria), "11", "31")]
-    [DataRow(typeof(Day02RedNosedReports), "2", "4")]
-    [DataRow(typeof(Day03MullItOver), "161", "48")]
-    [DataRow(typeof(Day04CeresSearch), "18", "9")]
+    [Test]
+    [Arguments(typeof(Day01HistorianHysteria), "11", "31")]
+    [Arguments(typeof(Day02RedNosedReports), "2", "4")]
+    [Arguments(typeof(Day03MullItOver), "161", "48")]
+    [Arguments(typeof(Day04CeresSearch), "18", "9")]
     public void CheckTestInputs(Type dayType, string part1, string part2) =>
-        Common.CheckDay(dayType, part1, part2, true);
+        _ = Common.CheckDay(dayType, part1, part2, true);
 }
